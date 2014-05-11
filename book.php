eae702840136fd5be5fdf416473bac8c50d92867
blackhawk89 <piotrek.stanior@gmail.com>
2014-05-11 12:55:28 +0200

    szkielet aplikacji

---
+++ book.php
@@ -0,0 +1,11 @@
+<?php
+
+class book{
+	private $id;
+	private $title;
+	private $author;
+
+	public function __construct(){
+		
+	}
+}
--- database.php
+++ database.php
@@ -4,15 +4,19 @@
  	private $user='root';
  	private $password='1234';
  	private $db='biblioteka';
- 	private $dbObj;
+
+ 	private $mysqliObj;
 
  	function __construct(){
- 		$this->dbObj = new mysqli($this->server,$this->user,$this->password,$this->db);
- 		if(mysql_errno()!=0){
- 			echo 'Bad connection error.';
- 			exit();
- 		}	
- 	}
+		@ $this->mysqliObj = new mysqli($this->server,$this->user,$this->password,$this->db);
+		if($this->mysqliObj->connect_errno){
+			exit('Bad connection');
+		}
+	}
+
+ 	public function query($queryString){
+		return $this->mysqliObj->query($queryString);
+	}
 
 
  }
